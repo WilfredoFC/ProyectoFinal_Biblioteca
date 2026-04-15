6502edcf27eb0c165e04082ce9cdc57317b65129
Wilfredo <wfelizcaba7@gmail.com>
2026-04-15 09:04:56 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoFinal_Biblioteca
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
